caaffcaf1dac8f4df043a216b76000957f2b461c
0_o <summer.wxy@gmail.com>
2016-11-14 10:38:07 +0800

    practice service

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Angular2
 
+
+
+
+----
+
 This project was generated with [angular-cli](https://github.com/angular/angular-cli) version 1.0.0-beta.19-3.
 
 ## Development server
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
 export class AppComponent {
-  title = 'app works!';
+
+  constructor(private heroService: HeroService) {}
+
+  title = 'Tour of Heroes';
+  selectedHero: Hero; 
+  heroes: Hero[];
+
+  getHeroes(): void {
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
 }
